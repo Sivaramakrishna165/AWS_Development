af9bd507185429b087210d8c1334df9c17eef106
sai <sai@gnail.com>
2024-11-11 20:28:11 +0530

    'updated'

--- aws_features/feature__config_rule_ebs_instance/lambda_functions/config_rule_ebs_instance_tags/index.py
+++
@@ -1,9 +0,0 @@
-#
-# Driver for VolumeTagEvaluationLambda.py
-#
-from config_instances_tags import (InstanceTagEvaluation)
-
-
-def lambda_handler(event, context):
-    evaluation = InstanceTagEvaluation()
-    return evaluation.handler_impl(event, context)
